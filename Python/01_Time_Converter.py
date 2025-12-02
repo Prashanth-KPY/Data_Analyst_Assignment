375da36166cf49063f83f9bf915ce89dca14a0e0
PRASHANTH <137322160+Prashanth-KPY@users.noreply.github.com>
2025-12-02 22:17:52 +0530

    small change

--- Python/01_Time_Converter.py
+++ Python/01_Time_Converter.py
@@ -4,7 +4,7 @@ def convert_minutes(n):
     mins = n % 60
     return f"{hrs} hr{'s' if hrs != 1 else ''} {mins} minutes"
 tmins=int(input("Enter minutes to convert: "))
-print(convert_minutes(tmins135))  
+print(convert_minutes(tmins))  
 
 
 
